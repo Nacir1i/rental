6a48c6fe4b92bcd3e901b32e5d6b9a43c64384cb
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-03-31 22:51:29 +0000

    create contact navbar

---
+++ components/ContactNav.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { SocialMediaLink } from '@/types';
+
+    const SOCIAL_MEDIA_LINKS: SocialMediaLink[] = [
+        {
+            icon: 'i-fa6-brands-facebook-f',
+            href: '/'
+        },
+        {
+            icon: 'i-fa6-brands-x-twitter',
+            href: '/'
+        },
+        {
+            icon: 'i-fa6-brands-instagram',
+            href: '/'
+        },
+    ]
+</script>
+
+<template>
+    <div class="w-full flex justify-center py-4 bg-black/60 border-b border-white">
+        <div class="w-[1170px] flex justify-between items-center">
+            <div class="flex items-center gap-3 text-white">
+                <Icon name="i-fa6-solid-phone" class="text-white text-xs" />
+                <p>06 XX XX XX XX</p>
+                <Icon name="i-fa6-solid-envelope" class="text-white text-xs" />
+                <p>lol.false@email.fr</p>
+            </div>
+            <div class="flex items-center gap-5">
+                <Icon class="text-white cursor-pointer" v-for="(link, index) in SOCIAL_MEDIA_LINKS" :key="index" :name="link.icon" />
+            </div>
+        </div>
+    </div>
+</template>
