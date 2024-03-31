6f0d62ddb8dc936670d37f175543e3730c64f6c5
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-03-31 23:05:36 +0000

    add responsiveness main navbar

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -23,10 +23,10 @@
 </script>
 
 <template>
-    <div class="sticky top-0 w-full flex justify-center items-center py-6 bg-gradient-to-b from-black/60 to-transparent">
+    <div class="sticky top-0 w-full flex justify-center items-center p-6 px-8 bg-gradient-to-b from-black/60 to-transparent">
         <div class="w-[1170px] flex justify-between items-center">
             <div class="w-32 h-10 bg-red-400" />
-            <ul class="flex items-center gap-5 uppercase text-sm">
+            <ul class="hidden items-center gap-5 uppercase text-sm lg:flex">
                 <li v-for="(link, index) in LINKS" :key="index">
                     <NuxtLink :to="link.href">{{ link.label }}</NuxtLink>
                 </li>
@@ -34,6 +34,9 @@
                     <Button variant="secondary" >+(212)6 xx xx xx xx</Button>
                 </li>
             </ul>
+            <span class="lg:hidden">
+                <Icon class="text-white text-xl" name="i-fa6-solid-align-justify" />
+            </span>
         </div>
     </div>
 </template>
