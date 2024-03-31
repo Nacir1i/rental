f59ea7f18b202e9fc0b7aac94a4053a9867b3537
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-03-31 22:55:40 +0000

    add responsiveness contact navbars

--- components/ContactNav.vue
+++ components/ContactNav.vue
@@ -18,15 +18,15 @@ import type { SocialMediaLink } from '@/types';
 </script>
 
 <template>
-    <div class="w-full flex justify-center py-4 bg-black/60 border-b border-white">
-        <div class="w-[1170px] flex justify-between items-center">
+    <div class="w-full flex justify-center p-4 bg-black/60 border-b border-white">
+        <div class="w-[1170px] flex justify-center lg:justify-between items-center">
             <div class="flex items-center gap-3 text-white">
                 <Icon name="i-fa6-solid-phone" class="text-white text-xs" />
                 <p>06 XX XX XX XX</p>
                 <Icon name="i-fa6-solid-envelope" class="text-white text-xs" />
                 <p>lol.false@email.fr</p>
             </div>
-            <div class="flex items-center gap-5">
+            <div class="hidden items-center gap-5 lg:flex">
                 <Icon class="text-white cursor-pointer" v-for="(link, index) in SOCIAL_MEDIA_LINKS" :key="index" :name="link.icon" />
             </div>
         </div>
