0f31b5a668c53aee2b4ff2491d314db0133025d4
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-02 21:56:05 +0000

    update main nav

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -23,20 +23,23 @@
 </script>
 
 <template>
-    <div class="sticky top-0 w-full flex justify-center items-center p-6 px-8 bg-gradient-to-b from-black/60 to-transparent">
-        <div class="w-[1170px] flex justify-between items-center">
-            <div class="w-32 h-10 bg-red-400" />
-            <ul class="hidden items-center gap-5 uppercase text-sm lg:flex">
-                <li v-for="(link, index) in LINKS" :key="index">
-                    <NuxtLink :to="link.href">{{ link.label }}</NuxtLink>
-                </li>
-                <li class="">
-                    <Button variant="secondary" >+(212)6 xx xx xx xx</Button>
-                </li>
-            </ul>
-            <span class="lg:hidden">
-                <Icon class="text-white text-xl" name="i-fa6-solid-align-justify" />
-            </span>
+    <div class="w-full absolute top-0 left-0 z-50">
+        <ContactNav />
+        <div class="w-full flex justify-center items-center p-6 px-8 bg-gradient-to-b from-black/60 to-transparent">
+            <div class="w-[1170px] flex justify-between items-center">
+                <div class="w-32 h-10 bg-red-400" />
+                <ul class="hidden items-center gap-5 uppercase text-sm lg:flex text-white">
+                    <li v-for="(link, index) in LINKS" :key="index">
+                        <NuxtLink :to="link.href">{{ link.label }}</NuxtLink>
+                    </li>
+                    <li class="">
+                        <Button variant="secondary" >+(212)6 xx xx xx xx</Button>
+                    </li>
+                </ul>
+                <span class="lg:hidden">
+                    <Icon class="text-white text-xl" name="i-fa6-solid-align-justify" />
+                </span>
+            </div>
         </div>
     </div>
 </template>
