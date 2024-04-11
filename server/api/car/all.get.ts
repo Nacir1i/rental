f172d1578939c068453f681bc381bdabcdbcad0d
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-11 04:14:07 +0000

    add types to get all cars api

--- server/api/car/all.get.ts
+++ server/api/car/all.get.ts
@@ -1,6 +1,27 @@
+import { Prisma } from "@prisma/client";
 import dbClient from "../../utils/prisma";
 
-export default defineEventHandler(async (_event) => {
-    const data = await dbClient.car.findMany();
-    return data;
-})
+const defaultSelect = {
+  id: true,
+  price: true,
+  mark: true,
+  model: true,
+  release_year: true,
+  fuel: true,
+  transmission: true,
+  distance_traveled: true,
+  createdAt: true,
+  updatedAt: true,
+  album: true,
+} satisfies Prisma.CarSelect;
+
+export type CarWithAlbum = Prisma.CarGetPayload<{
+  select: typeof defaultSelect;
+}>;
+
+export default defineEventHandler(async (_event): Promise<CarWithAlbum[]> => {
+  const data = await dbClient.car.findMany({
+    select: defaultSelect,
+  });
+  return data;
+});
