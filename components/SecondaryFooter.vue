68d30ac08ee8ea30290cc924aa86d6acb1439cbe
lolrandomxd <taoufiqnaciri234@gmail.com>
2024-04-14 07:41:31 +0100

    update footer links

--- components/SecondaryFooter.vue
+++ components/SecondaryFooter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import type { SocialMediaLink } from '@/types';
+    import type { NavLink, SocialMediaLink } from '@/types';
 
     const SOCIAL_MEDIA_LINKS: SocialMediaLink[] = [
         {
@@ -18,31 +18,32 @@
             label: "Instagram"
         },
     ]
+    const LINKS: NavLink[] = [
+        {
+            label: "home",
+            href: "/"
+        },
+        {
+            label: "our cars",
+            href: "/cars/listing"
+        },
+        {
+            label: "our news",
+            href: "/"
+        },
+        {
+            label: "contact",
+            href: "/"
+        }
+    ]
 </script>
 
 <template>
     <section class="bg-slate-100">
         <div class="max-w-screen-xl px-4 py-16 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
             <nav class="flex flex-wrap justify-center -mx-5 -my-2 uppercase">
-                <div class="px-5 py-2">
-                    <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                        our cars
-                    </a>
-                </div>
-                <div class="px-5 py-2">
-                    <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                        our news
-                    </a>
-                </div>
-                <div class="px-5 py-2">
-                    <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                        contact
-                    </a>
-                </div>
-                <div class="px-5 py-2">
-                    <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                        Terms
-                    </a>
+                <div class="px-5 py-2" v-for="(link, index) in LINKS" :key="index">
+                    <NuxtLink :to="link.href">{{ link.label }}</NuxtLink>
                 </div>
             </nav>
             <div class="flex justify-center mt-8 space-x-6">
